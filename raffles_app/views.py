974c0ae07b6125a5129cd1eb0cde3dfc89e518b7
carlos <carlosforero2025777@gamil.com>
2025-10-09 20:42:34 -0500

    u

--- raffles_app/views.py
+++ raffles_app/views.py
@@ -344,9 +344,9 @@ def checkout(request):
         f"&currency={currency}"
         f"&amount-in-cents={amount_in_cents}"
         f"&reference={reference}"
-        f"&redirect-url=http://localhost:8000/payment/return/"
+        f"&redirect-url=https://web-production-eb2c9.up.railway.app/payment/return/"  
         f"&customer-email={request.user.email}"
-        f"&signature-integrity={signature}"
+        f"&signature:{signature}"
     )
 
     return redirect(checkout_url)

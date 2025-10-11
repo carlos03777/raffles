035da627d555a5ca7fbf075887ea5f82d424d4f8
carlos <carlosforero2025777@gamil.com>
2025-10-11 08:49:51 -0500

    buket

--- raffles_site/settings.py
+++ raffles_site/settings.py
@@ -199,7 +199,7 @@ ALLOWED_HOSTS = [
 # ========= AWS S3 CONFIG ==========
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("raffles-app-20251110")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-2")
 
 # Opciones recomendadas

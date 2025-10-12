26b59f62a64c46683830511816a7e625ac6f3917
carlos <carlosforero2025777@gamil.com>
2025-10-12 10:27:31 -0500

    redireccion-de-signup

--- raffles_app/views.py
+++ raffles_app/views.py
@@ -173,6 +173,24 @@ def signup(request):
     return render(request, "raffles/signup.html", {"form": form})
 
 
+def activate(request, uidb64, token):
+    """Activa una cuenta tras verificar el token recibido por correo."""
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return render(request, "raffles/account_activation_success.html")
+    else:
+        return render(request, "raffles/account_activation_invalid.html")
+
+
+
 # def signup(request):
 #     """
 #     Registro de usuario con envío de correo de activación.
